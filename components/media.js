dda5d2bd51f72342e855fe4cbe26507ea2144c9b
Dan Laush <dan.laush@gmail.com>
2021-01-24 13:51:10 +0000

    Add media to projects

---
+++ components/media.js
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import styles from './media.module.css';
+
+const Media = ({src, alt, width, height, caption}) => (
+  <>
+    <div className={styles.mediaContainer}>
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        layout="responsive"
+        />
+    </div>
+    <p className={styles.caption}>{caption}</p>
+  </>
+)
+
+const MediaSection = ({src, alt, width, height, caption}) => (
+  <section className={styles.section}>
+    <Media src={src} alt={alt} width={width} height={height} caption={caption} />
+  </section>
+)
+
+export default Media;
+
+export {
+  Media,
+  MediaSection
+}
