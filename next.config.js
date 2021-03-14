d63c90ccfe622928a46c4a416aac5363278a98e0
Dan Laush <dan.laush@gmail.com>
2021-03-14 09:20:49 +0000

    Start on toc-per-article

--- next.config.js
+++ next.config.js
@@ -2,19 +2,39 @@ const rehypePrism = require('mdx-prism');
 const slug = require('rehype-slug')
 const toc = require("@jsdevtools/rehype-toc");
 
+const headings = [
+  'Give the people what they want, nothing more.',
+]
+
+// This is dumb but the easiest way I could
+// toggle the table of contents on/off per
+// blog post is to use the customise hook which
+// provides access to the current page's headings.
+// This config has the first heading of each
+// blog post which should show the ToC.
+const showToC = (tree) => {
+  try {
+    const heading = tree.children[0].children[0].children[0].children[0].value;
+    console.log('tree :>> ', );
+    return headings.includes(heading);
+  } catch(e) {
+    console.log(tree)
+    console.log(e);
+  }
+}
+
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [
-    ],
     rehypePlugins: [
       slug,
       [toc, {
         headings: ["h2"],
-        customizeTOC: function(tree) {
-          console.log('CUSTOMIZE TOC', tree);
-          tree.tagName = "TableOfContents";
-          return tree;
+        customizeTOC: (tree) => {
+          if(!showToC(tree)) return null;
+          return {
+            ...tree,
+            tagName: "TableOfContents",
+          }
         }
       }],
       rehypePrism,
