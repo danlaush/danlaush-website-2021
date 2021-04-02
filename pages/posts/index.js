dfa07ce5df9a4ffb8d6421003cf24c4fa4e358d8
Dan Laush <dan.laush@gmail.com>
2021-04-02 22:42:16 +0100

    Add Prometheus blog post

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,4 @@
-import { parseISO, getYear } from "date-fns";
+import { parseISO, getYear, compareDesc } from "date-fns";
 import Container from "../../components/container";
 import Layout from "../../components/layout";
 import TwoUp from "../../components/two-up";
@@ -9,18 +9,21 @@ const PostsIndex = ({ posts }) => {
   return (
     <Layout title="Web log" breadcrumb>
       <Container>
-        {Object.keys(posts).sort().reverse().map(year => (
-          <TwoUp title={year} key={year}>
-            <ul>
-            {posts[year].map((post) => (
-              <li key={post.slug}>
-                <a href={`/posts/${post.slug}`}>{post.title}</a>{" "}
-                {formatDate(post.date)}
-              </li>
-            ))}
-            </ul>
-          </TwoUp>
-        ))}
+        {Object.keys(posts)
+          .sort()
+          .reverse()
+          .map((year) => (
+            <TwoUp title={year} key={year}>
+              <ul>
+                {posts[year].map((post) => (
+                  <li key={post.slug}>
+                    <a href={`/posts/${post.slug}`}>{post.title}</a>{" "}
+                    {formatDate(post.date)}
+                  </li>
+                ))}
+              </ul>
+            </TwoUp>
+          ))}
       </Container>
     </Layout>
   );
@@ -28,12 +31,14 @@ const PostsIndex = ({ posts }) => {
 
 export async function getStaticProps() {
   const postsList = await listPosts();
-  const posts = sortPostsByYear(postsList);
+  const posts = organizePostsByYear(postsList);
   return {
     props: { posts },
   };
 }
 
+const sortPostsByYear = (postA, postB) =>
+  compareDesc(new Date(postA.date), new Date(postB.date));
 
 /**
  * {
@@ -42,13 +47,14 @@ export async function getStaticProps() {
  *   ]
  * }
  */
-function sortPostsByYear(postsList) {
+function organizePostsByYear(postsList) {
   return postsList.reduce((accumulator, post) => {
-    const year = getYear(parseISO(post.date))
+    const year = getYear(parseISO(post.date));
     accumulator[year] = accumulator[year] || [];
     accumulator[year].push(post);
+    accumulator[year] = accumulator[year].sort(sortPostsByYear);
     return accumulator;
-  }, {})
+  }, {});
 }
 
 export default PostsIndex;
