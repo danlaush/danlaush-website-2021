d0f757e78bbc9a2a1e20a4dcbb780b32b059ce4e
Dan Laush <dan.laush@gmail.com>
2022-07-27 20:00:50 +0100

    meta tags

--- components/layout.js
+++ components/layout.js
@@ -8,12 +8,22 @@ const Layout = ({ children, title, description, media, link, breadcrumb, htmlTit
     <Head>
       <title>{htmlTitle || title} | Dan Laush</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:site_name" content="Dan Laush" />
+      <meta property="og:title" content={htmlTitle || title} />
+      <meta name="twitter:title" content={htmlTitle || title} />
+      <meta name="theme-color" content="#7E4AD1"></meta>
       {/* <link rel="preload" href="/fonts/IBMPlexMono-Light-Latin1.woff2" as="font" />
       <link rel="preload" href="/fonts/IBMPlexSans-Regular-Latin1.woff2" as="font" />
       <link rel="preload" href="/fonts/IBMPlexSans-Medium-Latin1.woff2" as="font" />
       <link rel="preload" href="/fonts/IBMPlexSans-SemiBold-Latin1.woff2" as="font" /> */}
-      {description && <meta property="og:description" content={description} />}
-      {media && <meta property="og:image" content={`https://danlaush.biz${media}`} />}
+      {description && (<>
+        <meta property="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+      </>)}
+      {media && (<>
+        <meta property="og:image" content={`https://danlaush.biz${media}`} />
+        <meta name="twitter:image" content={`https://danlaush.biz${media}`} />
+      </>)}
     </Head>
     <PageHeader title={title} link={link} breadcrumb={breadcrumb} />
     {children}
--- components/post.js
+++ components/post.js
@@ -5,12 +5,13 @@ import TableOfContents from './table-of-contents';
 import styles from './post.module.css';
 // import '../styles/prism.css';
 
-const Post = ({ title, date, children, description }) => {
+const Post = ({ title, date, children, description, media }) => {
   return (
     <Project
       title={title}
       breadcrumb={{ url: "/posts", text: "Web log"}}
       description={`${formatDate(date)}${description ? `—${description}` : ''}`}
+      media={media ? media : undefined}
     >
       <Container>
         <div className={styles.post}>{children}</div>
